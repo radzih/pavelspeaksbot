b67f5306cbd800c77f807971b748d6a2beb0b238
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-05 09:38:59 +0300

    Add apscheduler

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler_di import ContextSchedulerDecorator
 
 from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
@@ -11,14 +13,15 @@ from tgbot.handlers.admin import register_admin
 from tgbot.handlers.echo import register_echo
 from tgbot.handlers.start import register_start_handlers
 from tgbot.handlers.test import register_test_handlers
+from tgbot.middlewares.scheduler import SchedulerMiddleware
 from tgbot.middlewares.db import DbMiddleware
 
 logger = logging.getLogger(__name__)
 
 
-def register_all_middlewares(dp):
+def register_all_middlewares(dp, scheduler):
     dp.setup_middleware(DbMiddleware())
-
+    dp.setup_middleware(SchedulerMiddleware(scheduler))
 
 def register_all_filters(dp):
     dp.filters_factory.bind(AdminFilter)
@@ -42,15 +45,19 @@ async def main():
     storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp = Dispatcher(bot, storage=storage)
+    scheduler = ContextSchedulerDecorator(AsyncIOScheduler())
+    scheduler.ctx.add_instance(bot, declared_class=Bot)
+
 
     bot['config'] = config
 
-    register_all_middlewares(dp)
+    register_all_middlewares(dp, scheduler)
     register_all_filters(dp)
     register_all_handlers(dp)
 
     # start
     try:
+        scheduler.start()
         await dp.start_polling()
     finally:
         await dp.storage.close()
