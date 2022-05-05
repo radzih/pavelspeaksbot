43a371d2222e49a75e27660c047a11e4232f58a6
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-05 18:21:15 +0300

    Configure django

--- admin_panel/database/admin.py
+++ admin_panel/database/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+# from .models import User
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('telegram_id','level')
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('telegram_id','level')
 
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
 # Register your models here.
