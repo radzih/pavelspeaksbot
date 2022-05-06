1db507f3ab27ac9586165e7ba593f6d75a9e501d
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-06 13:07:49 +0300

    Added  start for users who passed test

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 from aiogram_dialog import DialogManager
+from tgbot.filters.notregistered import NotRegistered
 
 from tgbot.keyboards.inline import start_test_markup
 from tgbot.widgets.states import ChooseCategories
@@ -10,6 +11,20 @@ async def user_start(message: Message) -> None:
         text="Hello, user!",
         reply_markup=start_test_markup)
 
+async def say_that_bot_already_started(
+    message: Message) -> None:
+    await message.asnwer(
+        text='Вы уже стартанули бота'
+    )
+
 def register_start_handlers(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands=["start"])
+    dp.register_message_handler(
+        user_start,
+        NotRegistered(),
+        commands=["start"])
+    dp.register_message_handler(
+        say_that_bot_already_started, 
+        commands=["start"]
+        )
+
     
