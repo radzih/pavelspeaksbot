57b4fac0f5fe5f8d1c43cdc132b38d943ab254c5
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-06 13:08:41 +0300

    Edited message

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -8,7 +8,7 @@ from tgbot.widgets.states import ChooseCategories
 
 async def user_start(message: Message) -> None:
     await message.answer(
-        text="Hello, user!",
+        text="Привет сначала нужно пройти тест",
         reply_markup=start_test_markup)
 
 async def say_that_bot_already_started(
