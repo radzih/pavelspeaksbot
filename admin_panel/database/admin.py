33d4ed2e54690506e3a4460310270049fce68f87
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-07 17:32:57 +0300

    Configured admin panel

--- admin_panel/database/admin.py
+++ admin_panel/database/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-
-# from .models import User
-
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('telegram_id','level')
+from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import Group
 
 
-# admin.site.register(User, UserAdmin)
+from .models import User, Word, Word_Category
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','telegram_id','level','learned_words')
+    list_filter = ('level',)
+    fields = ('telegram_id', 'level')
+
+    def learned_words(self, obj):
+        return len(obj.words.all())
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('id','word', 'translate', 'category', 'level')
+    list_filter = ('level','category')
+    search_fields = ('word__startswith',)
+
+class Word_CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','category', 'level')
+    list_filter = ('level',)
+    search_fields = ('category__startswith',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Word, WordAdmin)
+admin.site.register(Word_Category, Word_CategoryAdmin)
+admin.site.unregister(DjangoUser)
+admin.site.unregister(Group)
 # Register your models here.
--- admin_panel/database/models.py
+++ admin_panel/database/models.py
@@ -10,14 +10,23 @@ class Question(models.Model):
     correct_answer = models.CharField(max_length=255)
     point = models.IntegerField()
 
-        
-
 class Level(models.Model):
     level = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.level.capitalize()
 
 class Word_Category(models.Model):
     category = models.CharField(max_length=255)
     level = models.ForeignKey(Level, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ('id','category', 'level')
+        verbose_name_plural = "Word Categories"
+        verbose_name = 'Word Category'
+
+    def __str__(self):
+        return self.category.capitalize()
 
 class Word(models.Model):
     word = models.CharField(max_length=255)
@@ -26,9 +35,20 @@ class Word(models.Model):
     category = models.ForeignKey(Word_Category, on_delete=models.PROTECT)
     level = models.ForeignKey(Level, on_delete=models.PROTECT)
 
+    class Meta:
+        ordering = ('id','word', 'translate', 'category', 'level')
+
+    def __str__(self):
+        return self.word.capitalize()
+
 class User(models.Model):
     telegram_id = models.BigIntegerField(unique=True)
     level = models.ForeignKey(Level, on_delete=models.PROTECT, null=True)
     words = models.ManyToManyField(Word, blank=True)
     words_categories = models.ManyToManyField(Word_Category, blank=True)
 
+    class Meta:
+        ordering = ('id','level',)
+
+    def __str__(self):
+        return str(self.telegram_id)
