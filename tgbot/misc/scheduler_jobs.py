0b0cb3c174d80ef9da1f45ab2b32be2999132ebe
Radzih Vadim <iphonevadim2003@gmail.com>
2022-05-09 12:08:06 +0300

    Added job send_tip

--- tgbot/misc/scheduler_jobs.py
+++ tgbot/misc/scheduler_jobs.py
@@ -7,7 +7,7 @@ from aiogram.types.input_file import InputFile
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram_dialog.manager.bg_manager import BgManager
 
-from tgbot.services.db import db_get_random_word
+from tgbot.services.db import db_get_random_tip, db_get_random_word
 from tgbot.widgets.states import ChooseCategories
 
 
@@ -29,6 +29,19 @@ async def send_word(
             f'<b>{random_word.word.capitalize()} '
             f'- {random_word.translate.capitalize()}</b>'
             )
+    )
+
+async def send_tip(
+    bot: Bot,
+    telegram_id: int) -> None:
+    random_tip = await db_get_random_tip(
+        telegram_id=telegram_id
+    )
+    await bot.send_voice(
+        chat_id=telegram_id,
+        voice=InputFile(
+            path_or_bytesio=random_tip.audio_path
+        )
     )
 
 async def choose_words_categories(
@@ -77,7 +90,18 @@ async def add_jobs(
             },
         jitter=3000,
     )
-
+    scheduler.add_job(
+        send_tip,
+        id=f'{telegram_id}send_tip',
+        trigger='cron',
+        day_of_week='mon, wed, fri',
+        hour='14',
+        timezone=pytz.timezone('Europe/Moscow'),
+        kwargs={
+            'telegram_id': telegram_id,
+            },
+        jitter=6000,
+    )
     scheduler.add_job(
         choose_words_categories,
         id=f'{telegram_id}choose_words_categories',
